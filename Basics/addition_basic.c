44232af4a4e426bf6ac089d55b3bf71556a94e6e
Gayatri-Oman <oman.gayatree@gmail.com>
2026-03-09 22:58:16 +0530

    Add files via upload

---
+++ Basics/addition_basic.c
@@ -0,0 +1,17 @@
+// Basic program to Add Two Numbers
+
+#include<stdio.h>
+
+int main()
+{
+    int no1 = 11;
+    int no2 = 21;
+
+    int ans = 0;
+
+    ans = no1 + no2;
+
+    printf("Addition of %d and %d is %d\n",no1,no2,ans);
+
+    return 0;
+}
